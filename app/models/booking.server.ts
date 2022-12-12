d28bf9f03b8a1384ebf78191fc3340fc1ad141e2
Shagun Agrawal <agrawalshagun07@gmail.com>
2022-12-12 17:38:02 +0530

    create booking

---
+++ app/models/booking.server.ts
@@ -0,0 +1,34 @@
+import type { User, Transaction, Batch, Booking } from "@prisma/client";
+
+import cuid from "cuid";
+import { prisma } from "~/db.server";
+
+export async function createBooking({
+  batchId,
+  userId,
+}: { userId: User["id"] } & { batchId: Batch["id"] }) {
+  // Create booking and payment in one transaction
+  return await prisma.$transaction(async (tx) => {
+    // 1. Create corresponding transaction
+    const txn = await tx.transaction.create({
+      data: {
+        user_id: userId,
+      },
+    });
+
+    // 2. Create booking for this month
+    const booking = await tx.booking.create({
+      data: {
+        user_id: userId,
+        batch_id: batchId,
+        transaction_id: txn.id,
+      },
+    });
+
+    return booking;
+  });
+}
+
+export function getBatches() {
+  return prisma.batch.findMany();
+}
--- app/routes/class/book.tsx
+++ app/routes/class/book.tsx
@@ -1,3 +1,74 @@
+import { ActionArgs, LoaderArgs, redirect, json } from "@remix-run/node";
+import { createBooking, getBatches } from "~/models/booking.server";
+
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import { requireUserId } from "~/session.server";
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  const userId = await requireUserId(request);
+  const batchId = formData.get("batch_id");
+
+  if (typeof batchId !== "string" || batchId.length === 0) {
+    return json(
+      {
+        errors: {
+          batch_id: "Batch is required",
+        },
+      },
+      { status: 400 }
+    );
+  }
+
+  await createBooking({ userId, batchId });
+  return redirect(`/class`);
+}
+
+export async function loader({ request }: LoaderArgs) {
+  const batches = await getBatches();
+  return json({ batches });
+}
+
 export default function CreateBookingPage() {
-  return <div>Create booking page</div>;
+  const data = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
+
+  return (
+    <div>
+      Create a new booking for this month
+      <div className="flex"></div>
+      <Form method="post">
+        {data.batches.map((batch) => {
+          return (
+            <div className="m-2 flex" key={batch.id}>
+              <input
+                className="m-0"
+                type="radio"
+                id={batch.id}
+                name="batch_id"
+                value={batch.id}
+              />
+              <label
+                className="ml-2 inline align-top"
+                htmlFor={batch.id}
+              >{`${batch.start_time} - ${batch.end_time}`}</label>
+            </div>
+          );
+        })}
+
+        {actionData?.errors?.batch_id && (
+          <div className="pt-1 text-red-700" id="body-error">
+            {actionData.errors.batch_id}
+          </div>
+        )}
+
+        <button
+          type="submit"
+          className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+        >
+          Create Booking
+        </button>
+      </Form>
+    </div>
+  );
 }
